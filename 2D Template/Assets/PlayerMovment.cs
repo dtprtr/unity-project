b459321ab756fa35dedaeb792a151065a75ed0e6
APOLLO\gameb <189248david@gmail.com>
2025-09-15 18:02:11 -0700

    finished player movement and only jumps when on the ground

--- 2D Template/Assets/PlayerMovment.cs
+++ 2D Template/Assets/PlayerMovment.cs
@@ -5,27 +5,64 @@ public class PlayerMovment : MonoBehaviour
 {
     public float speed;
     public float JumpHeight;
-    public Rigidbody2D Rb;
+    private Rigidbody2D Rb;
     private float _movement;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    public Vector2 boxsize;
+    public float castDistance;
+    public LayerMask groundLayer;
 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        Rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Rb.linearVelocityX = _movement;
     }
 
-    public void Movement(InputAction.CallbackContext ctx)
+    public void Move(InputAction.CallbackContext ctx)
     {
-        _movement = ctx.ReadValue<Vector2>().x;
+
+        _movement = ctx.ReadValue<Vector2>().x * speed;
+
     }
 
     public void Jump(InputAction.CallbackContext ctx)
     {
+        if (ctx.ReadValue<float>() == 1)
+        {
+            if (IsGrounded())
+            {
+                Rb.linearVelocityY = JumpHeight;
+            }
+        }
+        
+            
 
     }
+
+// Check if the player is grounded using a boxcast
+    public bool IsGrounded()
+    {
+        if (Physics2D.BoxCast(transform.position, boxsize, 0, -transform.up, castDistance, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    } 
+    
+    // Draw the boxcast in the editor
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position + Vector3.down * castDistance, boxsize);
+    }
+
+    
 }
