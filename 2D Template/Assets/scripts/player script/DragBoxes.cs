c1d59f969481ccaf4cf90701c5975b6144120fc2
MUKILTEO\1725383 <189248david@gmail.com>
2025-09-17 13:30:17 -0700

    drag

---
+++ 2D Template/Assets/scripts/player script/DragBoxes.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class DragBoxes : MonoBehaviour 
+
+
+{
+    public LayerMask DragLayers;
+
+    [Range(0.0f, 100.0f)]
+    public float m_Damping = 1.0f;
+
+    [Range(0.0f, 100.0f)]
+    public float m_frequncy = 5.0f;
+
+    public bool m_DrawDragLine = true;
+    public Color m_color = Color.cyan;
+
+    private TargetJoint2D m_targtjoint;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var collider = Physics2D.OverlapPoint(worldPos, DragLayers);
+            // ! is used for negating a positve bool.
+            if (!collider)
+                return;
+
+
+            var body = collider.attachedRigidbody;
+            if (!body)
+                return;
+
+
+            // adds target to rigidbody2d
+            m_targtjoint = body.gameObject.AddComponent<TargetJoint2D>();
+            m_targtjoint.dampingRatio = m_Damping;
+            m_targtjoint.frequency = m_frequncy;
+
+            m_targtjoint.anchor = m_targtjoint.transform.InverseTransformPoint(worldPos);
+
+
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Destroy(m_targtjoint);
+            m_targtjoint = null;
+            return;
+        }
+
+        if (m_targtjoint)
+        {
+            m_targtjoint.target = worldPos;
+
+            if (m_DrawDragLine)
+            Debug.DrawLine (m_tar)
+
+            
+        }
+    }
+
+    
+}
